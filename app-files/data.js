b368d0b6a58cdaaea8ab5270a9c3d5d734de336b
coderHBB <developer@houseofbluebeans.com>
2021-02-11 08:51:04 +0530

    Added the project
    Adding the Project to the repository

---
+++ app-files/data.js
@@ -0,0 +1,55 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-patio360view",
+      "name": "Patio360View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 2.0942040666530106,
+        "pitch": 0.08049083646645094,
+        "fov": 1.265676047841681
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.430572278246519,
+          "pitch": 0.3903693240967119,
+          "title": "A",
+          "text": "Description"
+        },
+        {
+          "yaw": 1.7074264771708973,
+          "pitch": 0.282128798181029,
+          "title": "B",
+          "text": "Description"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
