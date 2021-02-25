5998437366982191dc28e34617026bf23ff8ac21
coderHBB <developer@houseofbluebeans.com>
2021-02-25 22:57:13 +0530

    updated data.js

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ var APP_DATA = {
           "title": "Elliot Creek Rectangle Outdoor Dining Table 40-in W x 66.9-in L with Umbrella Hole",
           "price": "$230.00",
           "text": "The high quality olefin fabric on the cushions of the Elliot Creek Metal Outdoor Dining Set are UV resistant to help keep your set looking its best. The table features a beautiful stamped pattern surrounded by tiles providing a brilliant visual appeal. Sturdy, durable steel frames form the foundation for a long-lasting and stylish dining set. Decorative, weather-resistant high-performance powder-coating covers all of the frames surfaces, creating a beautiful and appealing framework for the cushions. There are motion captains chairs for enhanced comfort. Deep seating set includes to complete the collection.",
-          "image":["https://dl.airtable.com/.attachmentThumbnails/45cdff3a345bb9ff57e02902277ba57f/d29a90e9","images/lamp.jpg"]
+          "image":["https://dl.airtable.com/.attachmentThumbnails/45cdff3a345bb9ff57e02902277ba57f/d29a90e9","http://mobileimages.lowes.com/product/converted/885227/885227010274_40321777.jpg?size=pdhi"]
         },
         {
           "yaw": 1.7074264771708973,
@@ -44,7 +44,7 @@ var APP_DATA = {
           "title": "allen + roth McAden Rectangle Wicker Outdoor Coffee Table 22-in W x 40-in L",
           "price":"$138.00",
           "text": "The traditional style of the McAden collection provides every-day comfort that matches most outdoor décor. This coffee table features an embossed wood-grain tabletop that is hand painted to look like a dark walnut slab of wood. A unique rounded-rectangle shape provides extra space for entertaining. The legs of the table are wrapped in warm brown hand-woven wicker. This coffee table can be combined with any of the McAden furniture available at Lowe’s.",
-          "image":["https://dl.airtable.com/.attachmentThumbnails/397b1a12bab37fd9c80270b06c02a44a/965b88ed","images/lamp.jpg","images/chair1.jpg","images/chair2.jpg","images/chair3.jpg","images/chair1.jpg"]
+          "image":["https://dl.airtable.com/.attachmentThumbnails/397b1a12bab37fd9c80270b06c02a44a/965b88ed"]
         }
       ]
     }
