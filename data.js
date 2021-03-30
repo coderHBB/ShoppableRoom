4ba5467d650dba146ba004d61da1938c21182e74
coderHBB <developer@houseofbluebeans.com>
2021-03-30 12:55:59 +0530

    Updating the hotspots

--- data.js
+++ data.js
@@ -71,12 +71,100 @@ var APP_DATA = {
           "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/500085/5000853111.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/500085/5000853111_40314204.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/500085/5000853111_40314207.jpg?size=pdhi"]
         },
         {
-          "yaw": 4.8,
-          "pitch": 0.4,
-          "title": "allen + roth McAden Rectangle Wicker Outdoor Coffee Table 22-in W x 40-in L",
-          "price":"$138.00",
-          "text": "The traditional style of the McAden collection provides every-day comfort that matches most outdoor décor. This coffee table features an embossed wood-grain tabletop that is hand painted to look like a dark walnut slab of wood. A unique rounded-rectangle shape provides extra space for entertaining. The legs of the table are wrapped in warm brown hand-woven wicker. This coffee table can be combined with any of the McAden furniture available at Lowe’s.",
-          "image":["models/PatioTable1099847.glb","https://dl.airtable.com/.attachmentThumbnails/397b1a12bab37fd9c80270b06c02a44a/965b88ed"]
+          "yaw": 6.25,
+          "pitch": 0.67,
+          "title": "allen + roth 8-in Green and White Indoor Artificial Succulent Plants",
+          "price": "$19.98",
+          "text": 'This artificial potted sisal leaf is good to bring the outdoor into your home. The green leaves give a realistic look and natural touch. The artificial stem can be easily set up on the table or shell and prefect to display for any room.',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/014434/014434066853.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/014434/014434066853_15738936.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/014434/014434066853_42511590.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 6.25,
+          "pitch": 0.9,
+          "title": "Grayson Lane Glam Blue Velvet Round Ottoman",
+          "price": "N/A",
+          "text": 'Add sleek texture with boho style to your bedroom or living room with this comfy large pouf, featuring aqua blue velvet upholstery. The large cushion can be used as a table, large ottoman, or stool, securely propped up by solid pine wood legs. The smooth top is taught with a radial stitched pattern and the sides are pleated for extra elegant texture',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/100313/1003130658.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/100313/1003130658_16578878.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 5.7,
+          "pitch": -0.05,
+          "title": "Safavieh Aurelia 63.5-in Antique Gold Floor Lamp",
+          "price": "$243.33",
+          "text": 'Historic photos of sophisticated five-star suites in London set the standards for global luxury and inspired the majestic Aurelia Floor Lamp. Its antique gold finish and white cotton shade accentuate the gentle curves of its metal base and body.',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/683726/683726437840.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/683726/683726437840_08486799.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 5.1,
+          "pitch": -0.15,
+          "title": "Mitzi by Hudson Valley Lighting Cora 5-in W 2-Light Aged Brass Modern/Contemporary Wall Sconce",
+          "price": "$220",
+          "text": 'Sconces are a throwback that remains vital in thoughtful design. Whether entertaining friends or staying in and catching up on you time, sconces set to a dimmer switch allow you to master the mood. Use them to playfully add personality to your space, taking it from functional to optimal along the way.',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/806134/806134835330.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/806134/806134835330_42293154.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 5.35,
+          "pitch": -0.05,
+          "title": "Cheung's Set of 2 Blue Wood Boxes with Overlaid Mirror Panels",
+          "price": "N/A",
+          "text": 'In bedrooms, entryways, mantles and more, keepsake boxes have provided a unique heirloom touch many spaces. Perfect for storing treasured items or everyday objects, they provide an eye-catching and warm look. This one in particular features a soft blue finish along with center and top mirrors overlaid by an elegant pattern.',
+          "image":["models/PatioTable1099833.glb","http://mobileimages.lowes.com/product/converted/100278/1002785820_15679299.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/100278/1002785820_15679301.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 5.38,
+          "pitch": 0.05,
+          "title": "Grayson Lane Grayson Lane 11-in x 12-in Eclectic Monkey Sculpture Gold Resin",
+          "price": "N/A",
+          "text": 'This product is not available',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/500031/5000313139.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/500031/5000313139_18188587.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 5.45,
+          "pitch": 0.05,
+          "title": "Umbra Prisma Matte Brass Picture Frame (8-in x 10-in)",
+          "price": "N/A",
+          "text": 'This product is not available',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/100244/1002445732.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/100244/1002445732_42488487.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 5.3,
+          "pitch": -0.2,
+          "title": "Grayson Lane Grayson Lane Set of 2 7.5-in, 9.75-in Farmhouse Vase Multi Colored Stoneware",
+          "price": "N/A",
+          "text": 'Evoking a warm and cozy feel, this set of small stoneware vases is an ideal decorative accent in living spaces of farmhouse or country cottage styled homes. The ceramic pair features a matte finish with pink geometric textured patterns around the body. Each vase features a narrow neck with the shoulders widening to the body in a pink finish. Place large leafy plants or small bouquets to elevate the old-world charm of farmhouse or country cottage style interiors.',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/500005/5000057489.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/500005/5000057489_17151697.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 7.25,
+          "pitch": 0.8,
+          "title": "allen + roth Jute Woven 5 x 8 Khaki Indoor Solid Farmhouse/Cottage Handcrafted Area Rug",
+          "price": "$114",
+          "text": 'Meticulously hand-woven by artisans, this 100% jute rug is not only beautiful but stain-resistant, making it perfect for high-traffic areas',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/889292/889292219273.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/889292/889292219273_42401185.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 7.8,
+          "pitch": 0.8,
+          "title": "Benzara Casual Pink and Gold Velvet Accent Chair",
+          "price": "N/A",
+          "text": 'Upscale your living room furniture by bringing this modernly designed accent chair in to your home. Supported by angled gold finished metal legs, this velvet upholstered accent chair features sea shell design backrest and deep vertical tufted detailing with extra plush padding that surrounds you in comfort.',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/100255/1002552112.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/100255/1002552112_16657415.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 6.3,
+          "pitch": 0,
+          "title": "NuWallpaper 30.75-sq ft Vinyl Floral Self-Adhesive Peel and Stick Wallpaper",
+          "price": "$41.66",
+          "text": 'Make a statement with this vibrant, vintage-inspired peel and stick wallpaper. Gorgeous botanical damasks comprised of yellow birds, coral flowers, and green roses pop against a rich teal background. Summer Love Teal Peel and Stick Wallpaper comes on one roll that measures 20.5 inches wide by 18 feet long.',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/091212/091212344044.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/091212/091212344044_11552946.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/091212/091212344044_11552947.jpg?size=pdhi"]
+        },
+        {
+          "yaw": 6.8,
+          "pitch": 0.45,
+          "title": "allen + roth 12.01-in W x 12.01-in H Cream Ceramic Planter",
+          "price": "$42.98",
+          "text": 'Durable and stylish, the 12 In. cream white ceramic Estes planter comes with an attached saucer. The Estes planter features a versatile design in a soft cream white to fit any room in your home.',
+          "image":["models/PatioTable1099833.glb","https://mobileimages.lowes.com/product/converted/037094/037094036691.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/037094/037094036691_10442813.jpg?size=pdhi","http://mobileimages.lowes.com/product/converted/037094/037094036691_10370194.jpg?size=pdhi"]
         },
       ]
     }
